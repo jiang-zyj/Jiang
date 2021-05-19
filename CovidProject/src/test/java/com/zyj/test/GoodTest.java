7465618acdf8fa889d400314d31b4912a1ef9b87
dr_glb <dr_glb@zyj.com>
2021-05-19 17:00:17 +0800

    test IDEA commit

---
+++ CovidProject/src/test/java/com/zyj/test/GoodTest.java
@@ -0,0 +1,11 @@
+package com.zyj.test;
+
+/**
+ * @ClassName GoodTest
+ * @Auther: YaJun
+ * @Date: 2021 - 05 - 19 - 16:43
+ * @Description: com.zyj.test
+ * @version: 1.0
+ */
+public class GoodTest {
+}
